41640fc503e557f56bce4652971f9f1bebb7a029
Tony Cui <tonycui@tonys-mbp-2.lan>
2022-05-05 22:30:10 -0400

    player movement controlled

---
+++ Explorers/Assets/playerBehavior.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerBehavior : MonoBehaviour
+{
+
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+
+    Vector2 movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+
+        /*
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        if (Input.GetKey(KeyCode.A))
+            rb.AddForce(Vector3.left);
+        if (Input.GetKey(KeyCode.D))
+            rb.AddForce(Vector3.right);
+        if (Input.GetKey(KeyCode.W))
+            rb.AddForce(Vector3.up);
+        if (Input.GetKey(KeyCode.S))
+            rb.AddForce(Vector3.down);
+        */
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+}
+
