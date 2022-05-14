c0852bedc25113a15c917e5ada9b8312781281f1
JonnyOnPC <jonathanzhaon@gmail.com>
2022-05-14 16:09:08 -0400

    Sound effect for restart level, created walking animation sprites
    Haven't actually implemented the animation sprites

--- Explorers/Assets/Scripts/Asta_WallBreak.cs
+++ Explorers/Assets/Scripts/Asta_WallBreak.cs
@@ -6,28 +6,30 @@ public class Asta_WallBreak : MonoBehaviour
 {
     GameObject asta;
     public Update_Bomb bombs;
-    AudioSource explosion;
+    AudioSource playAudio;
+    public AudioClip explosion;
+    
     // Start is called before the first frame update
     void Start()
     {
         asta = GameObject.Find("MainCharacter_Asta");
         GameObject bomb_ui = GameObject.Find("BombCount");
         bombs = (Update_Bomb) bomb_ui.GetComponent(typeof(Update_Bomb));
-        explosion = GetComponent<AudioSource>();
+     playAudio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void OnCollisionEnter2D(Collision2D other){
         if (other.gameObject.tag == "BreakableWall" && bombs.getBombs() > -1){
             //Freeze player for a few seconds
-            explosion.Play(0);
+         playAudio.PlayOneShot(explosion, 0.7F);
             StartCoroutine(freezePlayer());
         }
     }
         IEnumerator freezePlayer(){
         Debug.Log("Started Freeze Player Function");
         asta.GetComponent<playerBehavior>().enabled = false;
-        yield return new WaitForSeconds(2);
+        yield return new WaitForSeconds(1);
             asta.GetComponent<playerBehavior>().enabled = true;
             Debug.Log("Ended Freeze Player Function");
     }
--- Explorers/Assets/Scripts/RestartLevel.cs
+++ Explorers/Assets/Scripts/RestartLevel.cs
@@ -4,18 +4,30 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class RestartLevel : MonoBehaviour
 {
+    AudioSource playAudio;
+    public AudioClip restartLevelSound;
+    GameObject asta;
     // Start is called before the first frame update
     void Start()
     {
-        
+        asta = GameObject.Find("MainCharacter_Asta");
+        playAudio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKey(KeyCode.R)){
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            playAudio.PlayOneShot(restartLevelSound,0.4F);
+            StartCoroutine(restartScene());
             
         }
+    }
+    IEnumerator restartScene(){
+        Debug.Log("Restarting Level");
+        asta.GetComponent<playerBehavior>().enabled = false;
+        yield return new WaitForSeconds(1);
+        asta.GetComponent<playerBehavior>().enabled = true;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
